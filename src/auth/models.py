d66fc6cc22d747924dfcabf9b2adf2676dea961b
vladislav <vlad.gromov666@gmail.com>
2023-07-25 01:04:15 +0300

    Add user model

--- src/auth/models.py
+++ src/auth/models.py
@@ -23,3 +23,20 @@ role = Table(
     Column('created_at', TIMESTAMP, default=func.now()),
     Column('modified_at', TIMESTAMP, default=func.now(), onupdate=func.current_timestamp())
 )
+
+user = Table(
+    'user',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('username', String, nullable=False),
+    Column('email', String, nullable=False),
+    Column('first_name', String, nullable=True),
+    Column('last_name', String, nullable=True),
+    Column('middle_name', String, nullable=True),
+    Column('is_active', String, default=False, nullable=False),
+    Column('is_verified', String, default=False, nullable=False),
+    Column('is_superuser', String, default=False, nullable=False),
+    Column('created_at', TIMESTAMP, default=func.now()),
+    Column('modified_at', TIMESTAMP, default=func.now(), onupdate=func.current_timestamp()),
+    Column('role_id', Integer, ForeignKey(role.c.id))
+)
